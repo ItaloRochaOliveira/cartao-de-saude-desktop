6e315a40aa18c7c1c884a447192e26b78750d8ff
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2024-03-23 16:07:47 -0300

    feat: Add Vacina menu interaction and some interaction of Vacina controller

--- src/Controller/MenuApp.java
+++ src/Controller/MenuApp.java
@@ -1,53 +1,32 @@
 package Controller;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
 
 public class MenuApp {
-    // @FXML
-    // private BorderPane bp;
-
+   
     @FXML
     private AnchorPane ap;
 
     @FXML
-    public Menu opcoes;
-
-    // @FXML
-    // private void home(MouseEvent event){
-    //     bp.setCenter(ap);
-    // }
+    private AnchorPane optionPane;
 
     @FXML
-    public void handleMenuVacinaClick() {
-        loadPage(opcoes.getId());
+    public void handleMenuVacinaClick(ActionEvent event) {
+        MenuItem menu = (MenuItem) event.getTarget();
+
+        loadPage(menu.getId());
+        
     }
 
     public void loadPage(String page){
-        // Border root = null;
-
         try {
-            // System.out.println(getClass().getResource("/View/"+page + ".fxml"));
-            // System.out.println(ap.getId());
-
            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/"+page + ".fxml"));
            AnchorPane layout = loader.load();
 
@@ -58,15 +37,13 @@ public class MenuApp {
         } catch (IOException e) {
             System.out.println(e);
         }
-
-
     }
 
-    private void alert(){
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("ALerta");
-        alert.setHeaderText("Menu ");
-        alert.setContentText("Texto principal");
-        alert.show();
-    }
+    // private void alert(){
+    //     Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    //     alert.setTitle("ALerta");
+    //     alert.setHeaderText("Menu ");
+    //     alert.setContentText("Texto principal");
+    //     alert.show();
+    // }
 }
