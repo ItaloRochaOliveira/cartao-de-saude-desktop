cc07d126d28a93c0b46043d5260645d1b053a9ea
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2024-03-22 23:28:55 -0300

    feat: make the interactions menu and worked

--- src/Controller/MenuApp.java
+++ src/Controller/MenuApp.java
@@ -1,19 +1,65 @@
 package Controller;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 
 public class MenuApp {
-    @FXML
-    public Menu menuVacina;
+    // @FXML
+    // private BorderPane bp;
 
     @FXML
-    public void handleMenuVacinaClick(ActionEvent event) {
-        alert();
+    private AnchorPane ap;
+
+    @FXML
+    public Menu opcoes;
+
+    // @FXML
+    // private void home(MouseEvent event){
+    //     bp.setCenter(ap);
+    // }
+
+    @FXML
+    public void handleMenuVacinaClick() {
+        loadPage(opcoes.getId());
+    }
+
+    public void loadPage(String page){
+        // Border root = null;
+
+        try {
+            // System.out.println(getClass().getResource("/View/"+page + ".fxml"));
+            // System.out.println(ap.getId());
+
+           FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/"+page + ".fxml"));
+           AnchorPane layout = loader.load();
+
+            // Definir o novo layout carregado como o conteúdo do AnchorPane
+           ap.getChildren().clear();
+           ap.getChildren().add(layout);
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+
     }
 
     private void alert(){
